cf2dd03919755073ba50999430a15bb421a5f45b
Ulises Viña <playerlgam@live.com>
2021-04-11 14:08:47 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-const yesBtn = document.querySelector("#yes");
-const noBtn = document.querySelector("#no");
-
-const mvToRnd = () => {
-    noBtn.style.left = Math.floor(Math.random()*99).toString()+'px';
-    noBtn.style.top = Math.floor(Math.random()*99).toString()+'px';
+window.onload = _ => {
+    const yesBtn = document.querySelector("#yes");
+    const noBtn = document.querySelector("#no");
+    
+    const mvToRnd = _ => {
+        noBtn.style.left = Math.floor(Math.random()*99).toString()+'px';
+        noBtn.style.top = Math.floor(Math.random()*99).toString()+'px';
+    }
+    
+    yesBtn.onclick = _ => alert("Ya sabía que querías");
+    noBtn.onmouseover = _ => mvToRnd();
+    noBtn.onclick = _ => mvToRnd();   
 }
-
-yesBtn.onclick = _ => alert("Ya sabía que querías");
-noBtn.onmouseover = _ => mvToRnd();
-noBtn.onclick = _ => mvToRnd();
